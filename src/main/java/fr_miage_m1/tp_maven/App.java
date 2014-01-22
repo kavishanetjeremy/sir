dd228d6e580969c9703b59bb95243b2a62647b6a
10004388 <10004388@e214m10.istic.univ-rennes1.fr>
2014-01-22 09:15:48 +0100

    commit principale

---
+++ src/main/java/fr_miage_m1/tp_maven/App.java
@@ -0,0 +1,14 @@
+package fr_miage_m1.tp_maven;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    /**
+     * @param args 
+     */
+	private int i;
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
